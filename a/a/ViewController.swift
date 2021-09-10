0ab318712ac2c8a8289a5b2d6facf1f172f29e31
jiangjunhui <2806139037@qq.com>
2021-09-10 14:24:18 +0800

    添加日志打印

--- a/a/ViewController.swift
+++ a/a/ViewController.swift
@@ -14,15 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
-        let a = ["a": "1"]
-        let b = ["b": "2"]
-        let c = a + b
-        print(c)
-        
-        let d: Double = 1.23456
-        print(d.format(".2"))
-       
+      
         
     }
 
